4384cd4cdddf3de107163601c676b9c61e28a69d
luis.rodas <luis.rodas@digitalvaultstudio.com>
2019-07-14 17:31:54 -0700

    *check what screen must to initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:sms_alarm_flutter/pages/main.dart';
+import 'package:sms_alarm_flutter/pages/sms_commands.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+Future<bool> _checkIfExistValues() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String telephone = prefs.getString('telph');
+  if (telephone == null) return false;
+  if (telephone.isEmpty) return false;
+  return true;
+}
+
+class MyApp extends StatefulWidget {
+    @override
+    MyAppstate createState() => MyAppstate();
+}
+
+class MyAppstate extends State<MyApp> {
+
+  var _result;
+
+  @override
+  void initState() {
+     // This is the proper place to make the async calls
+     // This way they only get called once
+
+     // During development, if you change this code,
+     // you will need to do a full restart instead of just a hot reload
+        
+     // You can't use async/await here,
+     // We can't mark this method as async because of the @override
+     _checkIfExistValues().then((result) {
+         // If we need to rebuild the widget with the resulting data,
+         // make sure to use `setState`
+         setState(() {
+             _result = result;
+        });
+     });
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Check if there's exist prevusly telephone and password saved
+    Widget startWidget = MainPage();
+    if (_result == true) {
+        startWidget = SmsCommandPage();
+    }
+    
     return MaterialApp(
       title: 'SMS Alarm',
       theme: ThemeData(
@@ -21,7 +63,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MainPage(),
+      home: startWidget,
     );
   }
 }
