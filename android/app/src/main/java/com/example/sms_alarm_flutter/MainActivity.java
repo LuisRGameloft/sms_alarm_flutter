dee495bcddd1f4ba4ab197f1e7fe4985c6f20cbd
Luis Rodas <luis.rodas@gameloft.com>
2019-07-16 15:27:33 -0700

    +add code for SMS functionality

--- android/app/src/main/java/com/example/sms_alarm_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/sms_alarm_flutter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.sms_alarm_flutter;
 
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.telephony.SmsManager;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodCall;
@@ -20,15 +25,31 @@ public class MainActivity extends FlutterActivity {
         new MethodCallHandler() {
             @Override
             public void onMethodCall(MethodCall call, Result result) {
-              if(call.method.equals("p_SendSMS")){
-                String num = call.argument("phone");
-                String msg = call.argument("message");
+              if(call.method.equals("p_SendSMS")) {
+                if(isSmsPermissionGranted()) {
+                  String num = call.argument("phone");
+                  String msg = call.argument("message");
+                  try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(num, null, msg, null, null);
+                  } catch (Exception ex) { }
+                } else {
+                  requestSmsPermission();
+                }
                 result.success(null);
-              }else{
+              } else {
                 result.notImplemented();
               }
             }
         }
     );
+  }
+
+  public boolean isSmsPermissionGranted() {
+    return ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+  }
+
+  public void requestSmsPermission() {
+    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 100);
   }
 }
