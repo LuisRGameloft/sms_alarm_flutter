af005d209474055a2c7655d4c04c2fa412653b24
luis.rodas <luis.rodas@digitalvaultstudio.com>
2019-07-16 20:07:44 -0700

    +add common code

---
+++ lib/common/utils.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Future<bool> _exitAppPopup(BuildContext context) {
+  return showDialog(
+        context: context,
+        child: new AlertDialog(
+          title: new Text('Do you want to exit this application?'),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: new Text('No'),
+            ),
+            new FlatButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: new Text('Yes'),
+            ),
+          ],
+        ),
+      ) ??
+      false;
+  }
